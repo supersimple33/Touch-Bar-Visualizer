c158e4e053b1c5472ddc2be3561da6168d84dbda
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2021-08-04 10:05:56 -0400

    Added a new update notification

--- TouchBarVisualizer/AppDelegate.swift
+++ TouchBarVisualizer/AppDelegate.swift
@@ -18,6 +18,28 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		if #available(OSX 10.12.2, *) {
 			NSApplication.shared.isAutomaticCustomizeTouchBarMenuItemEnabled = true
 		}
+		
+		guard let releaseURL = URL(string: "https://api.github.com/repos/supersimple33/Touch-Bar-Visualizer/releases/latest") else {
+			print("We should never fail to create url")
+			return
+		}
+		do {
+			let html = try Data(contentsOf: releaseURL)
+			let htmlJson = try JSONSerialization.jsonObject(with: html, options: [])
+			if let htmlDict = htmlJson as? [String : Any] {
+				if let tag = htmlDict["tag_name"] as? String {
+					if tag > "v0.3.1" {
+						let alert = NSAlert()
+						alert.messageText = "There is a newer version of TBV available"
+						alert.runModal()
+					}
+				}
+			} else {
+				print("Failed to cover JSON")
+			}
+		} catch let error {
+			print(error)
+		}
 	}
 
 	func applicationWillTerminate(_ aNotification: Notification) {
